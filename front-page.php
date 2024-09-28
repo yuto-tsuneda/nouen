8efe018dc4f614550b5e9672e111713c5cf92f59
Yuto Tsuneda <yuto.tsuneda@prum.com>
2024-09-28 18:44:18 +0900

    about制作途中

--- front-page.php
+++ front-page.php
@@ -1,3 +1,25 @@
 <?php get_header('main'); ?>
-<h1>Hello</h1>
+
+<div class="about">
+  <div class="about__topPic">
+    <img class="about__topPic__left" src="<?php echo get_template_directory_uri(); ?>/assets/images/about-image01.webp" alt="やぎ">
+    <img class="about__topPic__mid" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.webp" alt="サイトロゴ">
+    <img class="about__topPic__bottom" src="<?php echo get_template_directory_uri(); ?>/assets/images/about-image02.webp" alt="トマト">
+  </div>
+  <div class="about__topDescription">
+    <p>自然の恵み農園は、<br>
+      自然の恵みと動物の尊さが調和する特別な場所です。<br>
+      新鮮で美味しい農産物を栽培し、心温まる動物たちと触れ合える場所でもあります。<br>
+    </p>
+  </div>
+  <div class="about__bottomPic">
+    <img class="about__bottomPic__left" src="<?php echo get_template_directory_uri(); ?>/assets/images/about-image03.webp" alt="農家">
+    <img class="about__bottomPic__right" src="<?php echo get_template_directory_uri(); ?>/assets/images/about-image04.webp" alt="牛">
+  </div>
+</div>
+
+
+
+
+
 <?php get_footer(); ?>
