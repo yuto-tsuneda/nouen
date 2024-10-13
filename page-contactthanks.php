2b5c013ed805838bb1289f0cacccf2f4035cf66a
Yuto Tsuneda <yuto.tsuneda@prum.com>
2024-10-14 00:56:25 +0900

    一通り完成URL系はまだ。

--- page-contactthanks.php
+++ page-contactthanks.php
@@ -12,10 +12,10 @@
   
   <div class="contact__thanks">
     <h3>お問い合わせありがとうございました。</h3>
-  <p class="contact__thanks__s">担当者より5営業日以内に返信いたします。<br>
-  お急ぎの場合は、お電話にてお問い合わせください。</p>
-  <p class="contact__thanks__bold">問い合わせ番号：<span class="contact__thanks__big">123-567-8901</span></p>
-  <p class="contact__thanks__bold">【受付時間】10:00 ~ 18:00（土日祝を除く）</p>
+  <p class="contact__thanks__s">担当者より<br class="display__sp">5営業日以内に返信いたします。<br>
+  お急ぎの場合は、<br class="display__sp">お電話にてお問い合わせください。</p>
+  <p class="contact__thanks__bold">問い合わせ番号：<br class="display__sp"><span class="contact__thanks__big">123-567-8901</span></p>
+  <p class="contact__thanks__bold">【受付時間】<br class="display__sp">10:00 ~ 18:00（土日祝を除く）</p>
 </div>
   <div class="contact__gotop">
     <a href="<?php echo home_url(); ?>">TOPに戻る</a>
